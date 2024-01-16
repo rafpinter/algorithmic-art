f770867312266b29288dbc9dd07778af4a30c367
Rafaela Pinter <rpinter811@gmail.com>
2024-01-16 14:16:57 -0500

    style circles

--- 01-first-sketch/circles.js
+++ 01-first-sketch/circles.js
@@ -1,41 +1,36 @@
-let stroke_col = 200;
-let fill_col = 30;
+// circle colors
+let fill_color_1 = 230;
+let fill_color_2 = 12;
+let fill_color_3 = 30;
 
 function setup() {
     wd = 600;
     ht = 600;
     createCanvas(wd, ht);
-    background(220);
+    background(250);
 }
 
 function draw() {
     for (let X = 0; X < 10; X++) {
         for (let Y = 0; Y < 10; Y++) {
-            for (let posX = 0; posX < 5; posX++) {
-                for (let posY = 0; posY < 5; posY++) {
-                    fill(fill_col);
-                    noStroke();
-                    circle(posX * 25 + 20 * X + 10, posY * 25 + 20 * Y + 10, 5);
-
-                    fill(fill_col * 2);
-                    noStroke();
-                    circle(posX * 25 + 20 * X + 100, posY * 25 + 20 * Y + 100, 5);
-
-                    fill(fill_col * 3);
-                    noStroke();
-                    circle(posX * 25 + 20 * X + 200, posY * 25 + 20 * Y + 200, 5);
-
-                    fill(fill_col * 4);
-                    noStroke();
-                    circle(posX * 25 + 20 * X + 300, posY * 25 + 20 * Y + 300, 5);
-                }
-            }
+            create_squares(X, Y);
         }
     }
 }
 
-// function not working
-function create_squares(position_shift, fill_mult, fill_col) {
-    fill(fill_col * fill_mult);
-    circle(posX * 25 + 20 * X + position_shift, posY * 25 + 20 * Y + position_shift, 5);
+function create_squares(X, Y) {
+    for (let posX = 0; posX < 5; posX++) {
+        for (let posY = 0; posY < 5; posY++) {
+            create_circles(1, 10, posX, posY, X, Y);
+            create_circles(2, 100, posX, posY, X, Y);
+            create_circles(3, 200, posX, posY, X, Y);
+            create_circles(4, 300, posX, posY, X, Y);
+        }
+    }
+}
+
+function create_circles(fill_shift, pos_shift, posX, posY, X, Y) {
+    fill(fill_color_1, fill_color_2, fill_color_3 * fill_shift);
+    noStroke();
+    circle(posX * 25 + 20 * X + pos_shift, posY * 25 + 20 * Y + pos_shift, 5);
 }
