cf8693919b2820ee6d7921084abaf0b56c66db3f
Rafaela Pinter <rpinter811@gmail.com>
2024-01-24 14:29:00 -0500

    renaming folder

--- 02-bauhaus/interactive-bauhaus-cube.js
+++ 02-bauhaus/interactive-bauhaus-cube.js
@@ -1,14 +1,52 @@
-// Cube variables
-let dim_cube = 4; // 5
+
+/*
+Genuary: Bauhaus
+
+"At the core of Bauhaus art were principles that championed simplicity and utility:
+- Unity of art and technology,
+- Clean, abstract styles,
+- Minimalism with emphasis on rationality."
+from: https://filmlifestyle.com/what-is-bauhaus/
+
+Inspirations:
+- Mondrian: 
+- https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.peoplesworld.org%2Farticle%2F100-years-of-bauhaus-building-for-a-society-of-equals%2F&psig=AOvVaw3R0SELCYlxOXtjsYoEcSwm&ust=1706206557542000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIiwnvHQ9oMDFQAAAAAdAAAAABAD
+
+Rafaela Pinter
+*/
+
+
+// Playground variables
+// --------------------
+
+// Cube dimensions
+let dim_cube = 3; // 5
+
+// Grid
+let ext_space = 50;
+let int_space = 20; // min: 5
+
+// Objects
+let round_edges = 5;
+let random_stroke = false;
+let transp = 250;
+let stroke_weight = 0;
+
+// Randomness
+let random_sizes = false;
+let inc_limit = 500;
+let no_stroke = false;
+let random_objects = false;
+let object_number = 0; // Square:0 | Circle:1 | Triangle:2
+
+
+// ---------------------
 
 // Global variables
 let wd = 800;
 let ht = 800;
-let ext_space = 40;
-let int_space = 10;
-let round_edges = 1;
 let color_array = [];
-let background_color = 240;
+let background_color = 245;
 let sq_size = (wd - ext_space * 2 - int_space * (dim_cube - 1)) / dim_cube;
 var cube;
 
@@ -16,21 +54,14 @@ var cube;
 function setup() {
     createCanvas(wd, ht);
     noLoop();
-    // frameRate(0.6);
 
     // probabilistic colors
     color_array = [
-        color(157, 39, 25), // red
-        color(21, 64, 132), // blue
-        color(34, 34, 34), // black
-        color(215, 180, 24), // yellow
-        color(240, 240, 240), // white
-        color(240, 240, 240), // white
-        color(240, 240, 240), // white
-        color(240, 240, 240), // white
-        color(240, 240, 240), // white
-        // color(240, 240, 240), // white
-        // color(240, 240, 240), // white
+        color(157, 39, 25, transp), // red
+        color(21, 64, 132, transp), // blue
+        color(34, 34, 34, transp), // black
+        color(215, 180, 24, transp), // yellow
+        color(background_color), // white
     ];
     cube = new Cube(dim_cube, ext_space, int_space, round_edges, color_array, sq_size);
 }
@@ -38,7 +69,7 @@ function setup() {
 // Draw function
 function draw() {
     background(background_color);
-    strokeWeight(4);
+    strokeWeight(stroke_weight);
     for (let i = 0; i < cube.objects.length; i++) {
         cube.objects[i].display(); // Changed from 'cube.squares[i]' to 'cube.objects[i]'
     }
@@ -72,7 +103,14 @@ class Cube {
         this.objects = []; // Renamed from 'this.squares' to 'this.objects'
         for (let i = 0; i < this.shape; i++) {
             for (let j = 0; j < this.shape; j++) {
-                let randint = floor(random(3)); // Use floor to get integer values
+                let randint;
+                if (random_objects) {
+                    randint = floor(random(3)); // Use floor to get integer values
+                }
+                else {
+                    randint = object_number;
+                }
+
                 if (randint === 0) {
                     // push square
                     this.objects.push(new Square(i, j, this.sq_size, this.round_edges, this.color_array, this.ext_space, this.int_space));
@@ -120,47 +158,69 @@ class GeometricForm {
 
 class Circle extends GeometricForm {
     display() {
+        if (random_stroke) {
+            noStroke();
+        }
+
+        let inc = 0;
+        if (random_sizes) {
+            inc = random(-inc_limit, inc_limit);
+        }
+
         fill(this.fill_color);
         circle(
             this.xmin + this.size / 2,
             this.ymin + this.size / 2,
-            this.size);
+            this.size + inc,
+        );
     }
 }
 
 class Triangle extends GeometricForm {
     display() {
-        // noStroke();
         fill(this.fill_color);
+
+        // controling stroke
+        if (no_stroke == false) {
+            stroke(0);
+        }
+        else {
+            noStroke();
+        }
+
         let randint = floor(random(3));
+
         if (randint === 0) {
+            if (random_stroke) {
+                noStroke();
+            }
             triangle(
-                this.xmin, // x1, left corner of the base
-                this.ymax, // y1, bottom
-                this.xmax, // x2, right corner of the base
-                this.ymax, // y2, bottom
-                (this.xmin + this.xmax) / 2, // x3, center of the base (now the tip)
-                this.ymin // y3, top (now the tip)
+                this.xmin,
+                this.ymax,
+                this.xmax,
+                this.ymax,
+                (this.xmin + this.xmax) / 2,
+                this.ymin
             );
         }
         else if (randint === 1) {
             triangle(
-                this.xmin, // x1
-                this.ymin, // y1
-                this.xmax, // x2
-                this.ymin, // y2
-                (this.xmin + this.xmax) / 2, // x3 (center of base)
-                this.ymax, // y3 (top of triangle)
+                this.xmin,
+                this.ymin,
+                this.xmax,
+                this.ymin,
+                (this.xmin + this.xmax) / 2,
+                this.ymax,
             );
         }
         else if (randint === 2) {
             triangle(
                 this.xmin,
-                this.ymin, // New left base
+                this.ymin,
                 this.xmin,
-                this.ymax, // New right base
+                this.ymax,
                 this.xmax,
-                (this.ymin + this.ymax) / 2 // New tip
+                (this.ymin + this.ymax) / 2
             );
         }
 
@@ -171,12 +231,33 @@ class Triangle extends GeometricForm {
 // Squares data structure
 class Square extends GeometricForm {
     display() {
-        // noStroke();
+        if (no_stroke == false) {
+            stroke(0);
+        }
+        else {
+            noStroke();
+        }
         fill(this.fill_color);
-        square(
-            this.xmin,
-            this.ymin,
-            this.size,
-            this.round_corner);
+
+        let inc = 0;
+        if (random_sizes) {
+            inc = random(-inc_limit, inc_limit);
+        }
+
+        if (round_edges == 0) {
+            square(
+                this.xmin,
+                this.ymin,
+                this.size + inc,
+            );
+        }
+        else {
+            square(
+                this.xmin,
+                this.ymin,
+                this.size + inc,
+                this.round_corner
+            );
+        }
     }
 }
