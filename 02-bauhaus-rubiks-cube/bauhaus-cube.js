3752e1ee701af15fd0ea464eb46bd971f6310af7
Rafaela Pinter <rpinter811@gmail.com>
2024-01-23 21:27:27 -0500

    bauhaus interactive

---
+++ 02-bauhaus-rubiks-cube/bauhaus-cube.js
@@ -0,0 +1,48 @@
+let wd = 500;
+let ht = 500;
+let ext_space = 40;
+let int_space = 10;
+let round_edges = 10;
+let color_array = [];
+let sq_space;
+
+let background_color = 200;
+
+function setup() {
+    createCanvas(wd, ht);
+    noLoop();
+    // frameRate(0.75);
+
+    sq_space = (wd - ext_space * 2 - int_space * 2) / 3;
+
+    color_array = [
+        color(157, 39, 25), // red
+        color(21, 64, 132), // blue
+        color(215, 180, 24), // yellow
+        color(237, 237, 237), // white
+        color(34, 34, 34) // black
+    ];
+
+}
+
+function draw() {
+    background(background_color);
+    noStroke();
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            print_square(i, j);
+        }
+    }
+}
+
+function print_square(i, j) {
+    fill(random(color_array));
+    square(
+        ext_space + i * (sq_space + int_space),
+        ext_space + j * (sq_space + int_space),
+        sq_space,
+        round_edges);
+
+}
+
+
---
+++ 02-bauhaus-rubiks-cube/interactive-bauhaus-cube.js
@@ -0,0 +1,107 @@
+let wd = 600;
+let ht = 600;
+let ext_space = 40;
+let int_space = 10;
+let round_edges = 10;
+let color_array = [];
+let background_color = 220;
+let sq_size = (wd - ext_space * 2 - int_space * 2) / 3;
+var cube;
+
+function setup() {
+    createCanvas(wd, ht);
+    noLoop();
+    // frameRate(0.75);
+    color_array = [
+        color(157, 39, 25), // red
+        color(21, 64, 132), // blue
+        color(215, 180, 24), // yellow
+        color(237, 237, 237), // white
+        color(34, 34, 34) // black
+    ];
+}
+
+function draw() {
+    background(background_color);
+    let dim_cube = 3; // 3x3 cube
+    cube = new Cube(dim_cube, ext_space, int_space, round_edges, color_array, sq_size);
+    for (let i = 0; i < dim_cube * dim_cube; i++) {
+        cube.squares[i].display();
+    }
+}
+
+function mousePressed(event) {
+    if (event.button === 0) {
+        let x = Math.floor(mouseX / 200);
+        let y = Math.floor(mouseY / 200);
+        if (x == 1) {
+            1
+            y += 2;
+        }
+        if (x == 2) {
+            y += 4;
+        }
+        cube.squares[x + y].redraw_square();
+    }
+    if (event.button === 1 || event.button === 2) {
+        redraw();
+    }
+}
+
+class Cube {
+    constructor(shape, ext_space, int_space, round_edges, color_array, sq_size) {
+        this.shape = shape;
+        this.ext_space = ext_space;
+        this.int_space = int_space;
+        this.round_edges = round_edges;
+        this.color_array = color_array;
+        this.sq_size = sq_size;
+        this.create_squares();
+    }
+
+    create_squares() {
+        this.squares = [];
+        for (let i = 0; i < this.shape; i++) {
+            for (let j = 0; j < this.shape; j++) {
+                this.squares.push(new Square(i, j, this.sq_size, this.round_edges, this.color_array, this.ext_space, this.int_space));
+            }
+        }
+    }
+}
+
+class Square {
+    constructor(x, y, size, round_corner, color_array, ext_space, int_space) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.round_corner = round_corner;
+        this.color_array = color_array;
+        this.ext_space = ext_space;
+        this.int_space = int_space;
+        this.dimensions();
+    }
+
+    dimensions() {
+        this.xmin = this.ext_space + this.x * (this.size + this.int_space);
+        this.xmax = this.xmin + this.size;
+        this.ymin = this.ext_space + this.y * (this.size + this.int_space)
+        this.ymax = this.ymin + this.size;
+
+        this.fill_color = random(this.color_array);
+    }
+
+    display() {
+        noStroke();
+        fill(this.fill_color);
+        square(
+            this.xmin,
+            this.ymin,
+            this.size,
+            this.round_corner);
+    }
+
+    redraw_square() {
+        this.fill_color = random(this.color_array);
+        this.display();
+    }
+}
