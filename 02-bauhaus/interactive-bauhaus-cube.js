2a84f1a489ef0dc248f36c889570967deaaa0539
Rafaela Pinter <rpinter811@gmail.com>
2024-01-29 23:35:07 -0500

    bauhaus picture

--- 02-bauhaus/interactive-bauhaus-cube.js
+++ 02-bauhaus/interactive-bauhaus-cube.js
@@ -1,6 +1,5 @@
-
 /*
-Genuary: Bauhaus
+Genuary: Bauhaus (?)
 
 "At the core of Bauhaus art were principles that championed simplicity and utility:
 - Unity of art and technology,
@@ -8,89 +7,144 @@ Genuary: Bauhaus
 - Minimalism with emphasis on rationality."
 from: https://filmlifestyle.com/what-is-bauhaus/
 
-Inspirations:
-- Mondrian: 
-- https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.peoplesworld.org%2Farticle%2F100-years-of-bauhaus-building-for-a-society-of-equals%2F&psig=AOvVaw3R0SELCYlxOXtjsYoEcSwm&ust=1706206557542000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIiwnvHQ9oMDFQAAAAAdAAAAABAD
+What if I:
+- recreate my rubik's cube in a bauhaus style?
+- can make my cube bigger?
+- use probability to remove some objects?
+- change the objects and their sizes randomly?
+- change the transparancy?
+- remove some objects and add random strokes?
+- create some projections?
+
+Is it already Bauhaus?
 
 Rafaela Pinter
 */
-
 
 // Playground variables
 // --------------------
 
 // Cube dimensions
-let dim_cube = 3; // 5
+let dim_cube = 10;
 
-// Grid
-let ext_space = 50;
-let int_space = 10; // min: 5
+// Objects Grid
+let ext_space = 100;
+let int_space = 10;
 
-// Objects
+// Objects perimeter
 let round_edges = 0;
-let transp = 250;
-let stroke_weight = 5;
-
-// Randomness
-let random_sizes = true;
-let inc_limit = 250;
 let no_stroke = false;
-let random_stroke = true;
-let random_objects = true;
-let object_number = 0; // Square:0 | Circle:1 | Triangle:2
-// ---------------------
+let stroke_weight = 0;
+let stroke_color = 0;
 
-// Global variables
+// Object color
+let transp = 40;
+let no_fill = false;
+
+// Objects repetition
+let max_forms_square = 10;
+let max_forms_triangle = 10;
+let max_forms_circles = 10;
+
+// Objects Randomness
+let random_sizes = true;
+let inc_limit = 100;
+let random_stroke = true;
+let random_objects = false;
+let object_number = 0; // Square:0 | Circle:1 | Triangle:2
+
+// Hidden objects
+let max_random_number = 5;
+
+// Text
+let add_text = true;
+
+// Background
+let mode = "random_triagles";
+let r = 237;
+let g = 237;
+let b = 237;
+
+
+// Canvas variables 
+// ---------------
 let wd = 800;
-let ht = 800;
+let ht = 900;
 let color_array = [];
-let background_color = 245;
 let sq_size = (wd - ext_space * 2 - int_space * (dim_cube - 1)) / dim_cube;
 var cube;
+
+
 
 // Setup function
 function setup() {
     createCanvas(wd, ht);
     noLoop();
 
-    // probabilistic colors
+    // Probabilistic colors
     color_array = [
-        color(166, 41, 13, transp), // red
-        color(4, 119, 191, transp), // blue
+        // color(157, 39, 25, transp), // red
+        // color(21, 64, 132, transp), // blue
+        // color(215, 180, 24, transp), // yellow
+        // color(237, 237, 237, transp), // white
         color(34, 34, 34, transp), // black
-        color(242, 183, 5, transp), // yellow
-        color(background_color, transp), // white
-        color(background_color, transp), // white
-        color(background_color, transp), // white
-        // color(background_color, transp), // white
-        // color(background_color, transp), // white
-        // color(background_color), // white
-        color(background_color), // white
-        // color(background_color), // white
-        // color(background_color), // white
-        // color(background_color), // white
+        // color(249, 91, 19), // orange
+        // color(245, 245, 245), // white no transp
     ];
-    cube = new Cube(dim_cube, ext_space, int_space, round_edges, color_array, sq_size);
+
+    cube = new Cube(
+        dim_cube,
+        ext_space,
+        int_space,
+        round_edges,
+        color_array,
+        sq_size);
 }
 
 // Draw function
 function draw() {
-    background(background_color);
+    if (mode == "dark") {
+        r = 40; g = 40; b = 40;
+    }
+    else if (mode == "random") {
+        r = random(0, 255); g = random(0, 255); b = random(0, 255);
+    }
+    else if (mode == "random_triangles") {
+        r = 110; g = 187; b = 222;
+    }
+    background(color(r, g, b));
+    stroke(stroke_color);
     strokeWeight(stroke_weight);
     for (let i = 0; i < cube.objects.length; i++) {
         cube.objects[i].display();
     }
+    txtBottom();
 }
 
-// Interactive function
-function mousePressed(event) {
-    if (event.button === 0) {
-        let x = Math.floor(mouseX / (wd / dim_cube));
-        let y = Math.floor(mouseY / (ht / dim_cube));
-        cube.objects[dim_cube * x + y].redraw_object();
-    }
-    if (event.button === 1) {
-        redraw();
+function txtBottom() {
+    font = "awanzaman";
+    textFont(font);
+    if (add_text) {
+        if (mode == "dark") {
+            cor = 200;
+        } else {
+            cor = 30;
+        }
+        push();
+        // Line
+        translate(0, ht * 0.875);
+        stroke(cor);
+        strokeWeight(ht * 0.005);
+        strokeCap(SQUARE);
+        line(100, 0, 700, 0)
+        noStroke();
+
+        // Text
+        fill(cor, cor, cor, 255);
+        textAlign(100);
+        textSize(ht * 0.025);
+        text("is it already bauhaus?", 100, ht * 0.031);
+        pop();
     }
 }
 
@@ -107,12 +161,12 @@ class Cube {
     }
 
     create_objects() {
-        this.objects = []; // Renamed from 'this.squares' to 'this.objects'
+        this.objects = [];
         for (let i = 0; i < this.shape; i++) {
             for (let j = 0; j < this.shape; j++) {
                 let randint;
                 if (random_objects) {
-                    randint = floor(random(3)); // Use floor to get integer values
+                    randint = floor(random(max_random_number));
                 }
                 else {
                     randint = object_number;
@@ -121,15 +175,39 @@ class Cube {
 
                 if (randint === 0) {
                     // push square
-                    this.objects.push(new Square(i, j, this.sq_size, this.round_edges, this.color_array, this.ext_space, this.int_space));
+                    this.objects.push(new Square(
+                        i,
+                        j,
+                        this.sq_size,
+                        this.round_edges,
+                        this.color_array,
+                        this.ext_space,
+                        this.int_space
+                    ));
                 }
                 else if (randint === 1) {
                     // push circle
-                    this.objects.push(new Circle(i, j, this.sq_size, this.round_edges, this.color_array, this.ext_space, this.int_space));
+                    this.objects.push(new Circle(
+                        i,
+                        j,
+                        this.sq_size,
+                        this.round_edges,
+                        this.color_array,
+                        this.ext_space,
+                        this.int_space
+                    ));
                 }
                 else if (randint === 2) {
                     // push triangle
-                    this.objects.push(new Triangle(i, j, this.sq_size, this.round_edges, this.color_array, this.ext_space, this.int_space));
+                    this.objects.push(new Triangle(
+                        i,
+                        j,
+                        this.sq_size,
+                        this.round_edges,
+                        this.color_array,
+                        this.ext_space,
+                        this.int_space
+                    ));
                 }
             }
         }
@@ -182,21 +260,23 @@ class GeometricForm {
 
 class Circle extends GeometricForm {
     display() {
-        // Randomness
-        // if (random_stroke || no_stroke == true) {
-        //     noStroke();
-        // }
         let inc = 0;
         if (random_sizes) {
             inc = random(-inc_limit, inc_limit);
         }
-        // Painting object
-        fill(this.fill_color);
-        circle(
-            this.xmin + this.size / 2,
-            this.ymin + this.size / 2,
-            this.size + inc,
-        );
+        for (let i = 0; i < max_forms_circles; i++) {
+            if (no_fill) {
+                noFill();
+
+            } else {
+                fill(this.fill_color);
+            }
+            circle(
+                this.xmin + this.size / 2 + i,
+                this.ymin + this.size / 2 + i,
+                this.size + inc,
+            );
+        }
     }
 }
 
@@ -204,8 +284,7 @@ class Triangle extends GeometricForm {
     display() {
         // Triangle points
         let x1, y1, x2, y2, x3, y3;
-
-        // Default triangle        
+        // Default triangle
         x1 = this.xmin;
         y1 = this.ymax;
         x2 = this.xmax;
@@ -213,7 +292,6 @@ class Triangle extends GeometricForm {
         x3 = (this.xmin + this.xmax) / 2;
         y3 = this.ymin;
 
-        // Declaring randint before using it
         let randint = floor(random(3));
 
         // Other side
@@ -232,20 +310,28 @@ class Triangle extends GeometricForm {
         }
 
         // Controling stroke
-        if (random_stroke || !no_stroke) {
+        if (random_stroke || no_stroke) {
             noStroke();
         }
 
-        // Displaying figure
-        fill(this.fill_color);
-        triangle(
-            x1,
-            y1,
-            x2,
-            y2,
-            x3,
-            y3
-        );
+        for (let i = 0; i < max_forms_triangle; i++) {
+            // Displaying figure
+            if (no_fill) {
+                noFill();
+            }
+
+            else {
+                fill(this.fill_color);
+            }
+            triangle(
+                x1 - i,
+                y1 - i,
+                x2 - i,
+                y2 - i,
+                x3 - i,
+                y3 - i
+            );
+        }
     }
 }
 
@@ -254,27 +340,44 @@ class Triangle extends GeometricForm {
 class Square extends GeometricForm {
     display() {
 
-        fill(this.fill_color);
-
         let inc = 0;
         if (random_sizes) {
             inc = random(-inc_limit, inc_limit);
         }
+        for (let i = 0; i < max_forms_square; i++) {
+            // Drawing object        
+            if (no_fill) {
+                noFill();
 
-        if (round_edges == 0) {
-            rect(
-                this.xmin,
-                this.ymin,
-                this.size + inc,
-            );
-        }
-        else {
-            rect(
-                this.xmin,
-                this.ymin,
-                this.size + inc,
-                this.round_corner
-            );
+            } else {
+                fill(this.fill_color);
+            }
+
+            if (round_edges != 0) {
+                rect(
+                    this.xmin + i,
+                    this.ymin + i,
+                    this.size + inc + i,
+                    this.size + inc + i,
+                    this.round_corner
+                );
+            }
+            else {
+                rect(
+                    this.xmin + i,
+                    this.ymin + i,
+                    this.size + inc,
+                );
+            }
         }
     }
 }
+
+
+
+
+
+
+/*
+the figures, I don't know. but the code surely is.
+*/
