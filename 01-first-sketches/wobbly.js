432089c9b54710fc2539bda6d31b740c2578af07
Rafaela Pinter <rpinter811@gmail.com>
2024-01-30 08:55:19 -0500

    adding sketch

---
+++ 01-first-sketches/wobbly.js
@@ -0,0 +1,78 @@
+let stroke_col = 200;
+
+function setup() {
+    wd = 1300;
+    ht = 750;
+    createCanvas(wd, ht);
+    background(20);
+}
+
+function draw() {
+    let min = 1; // 2
+    let max = 5; // 3
+    let a = 2; // 1 | 0.5
+    let b = 0.9; // 2 | 3
+    let i_max = random(min, max);
+    let j = random(min, max);
+    for (let i = 1; i < i_max; i++) {
+        graphFunction(x => sin(a * x ** b + sin(i * x ** b + 2 * sin(i * (x + 10) ** b))), 0, 3 * PI);
+    }
+    noLoop();
+}
+
+
+// Not my code from here:
+// Credits to https://www.youtube.com/watch?v=Q9Gje2vh22Q
+function graphFunction(fn, x1, x2) {
+    let o = getFunctionValues(fn, x1, x2, 5000); // genuary jan 16
+
+    let values = o.values;
+    let y1 = o.min;
+    let y2 = o.max;
+
+    drawGraph(values, x1, x2, y1, y2);
+}
+
+function drawGraph(values, x1, x2, y1, y2) {
+    stroke(stroke_col);
+    strokeWeight(1);
+
+    for (let i = 0; i < values.length - 1; i++) {
+        let x = values[i].x;
+        let y = values[i].y;
+
+        let screenX = map(x, x1, x2, 0, width - 1);
+        let screenY = map(y, y1, y2, height - 1, 0);
+
+        let xNext = values[i + 1].x;
+        let yNext = values[i + 1].y;
+
+        let screenXNext = map(xNext, x1, x2, 0, width - 1);
+        let screenYNext = map(yNext, y1, y2, height - 1, 0);
+
+        line(screenX, screenY, screenXNext, screenYNext);
+    }
+}
+
+function getFunctionValues(fn, x1, x2, n) {
+    let ar = [];
+    let min = Infinity;
+    let max = -Infinity;
+
+    let step = (x2 - x1) / n;
+
+    for (let x = x1; x <= x2; x += step) {
+        let y = fn(x);
+        ar.push({ x, y });
+
+        if (y < min) {
+            min = y;
+        }
+
+        if (y > max) {
+            max = y;
+        }
+    }
+
+    return { values: ar, min, max };
+}
