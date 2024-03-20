fb7cb5cfe658e7cc6b8f10c23585dbba124eb3ba
Rafaela Pinter <rpinter811@gmail.com>
2024-03-20 14:09:49 -0400

    defining plotter scale

--- 07-vernissage/bezier.js
+++ 07-vernissage/bezier.js
@@ -10,20 +10,38 @@ new_point = point
 vec_of_bezier_lines[i % len(vec)] = new_point
 */
 
-let pause = false
+// https://github.com/rethread-studio/algorithmic-art-course/blob/main/examples/template-p5-to-svg.html
+//letter: 8.5in x 11in
+//96dpi is for plotting on the UUNA TEK iDraw
+//which gives this width and height for a letter format paper
+//w=96*8.5=816
+//h=96*11=1056
+var echelle = 1
+var w = 816 * echelle
+var h = 1056 * echelle
+//I suggest to keep a bit of white space on the sides of page when plotting.
+//so plot within the area leftmargin - rightmargin on the x-axis and topmargin - bottommargin on the y axis
+var rightmargin = 0.9 * w
+var leftmargin = 0.1 * w
+var topmargin = 0.1 * h
+var bottommargin = 0.9 * h
+var actualwidth = rightmargin - leftmargin
+var actualheight = bottommargin - topmargin
+
+// playground
 let x1, y1, x2, y2, x3, y3, x4, y4
 let i = 30
 let offset = 1
-let speed = 0.006
+let speed = 0.01
 let j1 = -100
 let j2 = -100
 let j3 = 100
 let j4 = 100
 
-let steps = 150
+let steps = 100
 
 function setup() {
-    createCanvas(700, 900, SVG)
+    createCanvas(w, h, SVG)
     background(255)
 }
 
@@ -33,8 +51,8 @@ function draw() {
     // Tests:
     call_bezier(1)
     // save
-    let button = createButton('save svg')
-    button.position(750, 900)
+    let button = createButton('SAVE SVG')
+    button.position(w + 30, actualheight)
     button.mousePressed(save_svg)
 }
 
@@ -71,8 +89,14 @@ function bezier_func(l) {
         // Option 1
         // line(x2, windowWidth, x2, y2)
         // line(x3, y3, windowHeight, y3)
-        line(0, x1, x1, y1)
-        line(x4, y4, y4, windowHeight)
+        line(leftmargin, x1, x1, y1)
+        let max_right
+        if (y4 > rightmargin) {
+            max_right = rightmargin
+        } else {
+            max_right = y4
+        }
+        line(x4, y4, max_right, bottommargin)
 
         // Option 2
         // line(x1, y1)
