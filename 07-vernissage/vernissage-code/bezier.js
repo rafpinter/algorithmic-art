89f3a7267462341bbfe10b4a8eeafe577d90dada
Rafaela Pinter <rpinter811@gmail.com>
2024-03-27 21:39:51 -0400

    vernissage code

--- 07-vernissage/vernissage-code/bezier.js
+++ 07-vernissage/vernissage-code/bezier.js
@@ -24,16 +24,16 @@ var actualheight
 let x1, y1, x2, y2, x3, y3, x4, y4
 let i = 100
 let offset = 2
-let speed
+let noise_speed = 0.006
 let j1 = -100
 let j2 = -100
 let j3 = 100
 let j4 = 100
 
-let steps = 200
+let steps = 150
+var pause = false
 
 function setup() {
-
     w = windowWidth
     h = windowHeight
     rightmargin = 0.9 * w
@@ -43,116 +43,49 @@ function setup() {
     actualwidth = rightmargin - leftmargin
     actualheight = bottommargin - topmargin
     createCanvas(w, h)
-    clear()
-    // noLoop()
-    frameRate(0.5)
+    frameRate(20)
+    mousePressed()
 }
 
 function draw() {
-    bezier_func(1)
-    draw_text()
+    bezier_func()
+    // noLoop()
 }
 
-function bezier_func(l) {
+function bezier_func() {
     noFill()
     strokeWeight(1.5)
-    background(0)
-    let choice = random([0, 1, 2])
-    let r1 = random(0, 255)
-    let r2 = random(0, 255)
-    let g1 = random(0, 255)
-    let g2 = random(0, 255)
-    let b1 = random(0, 255)
-    let b2 = random(0, 255)
-    speed = random(0.003, 0.006)
-    if (choice != 2) {
-        for (let j = 0; j < steps; j++) {
 
-            x1 = (noise(offset + i) * width + j1) * l
-            y1 = (noise(offset + i + 2) * height + j1) * l
-            x2 = (noise(offset + i + 3) * width + j2) * l
-            y2 = (noise(offset + i + 4) * height + j2) * l
-            x3 = (noise(offset + i + 5) * width + j3) * l
-            y3 = (noise(offset + i + 6) * height + j3) * l
-            x4 = (noise(offset + i + 7) * width + j4) * l
-            y4 = (noise(offset + i + 8) * height + j4) * l
+    x1 = (noise(offset + i) * width + j1)
+    y1 = (noise(offset + i + 2) * height + j1)
+    x2 = (noise(offset + i + 3) * width + j2)
+    y2 = (noise(offset + i + 4) * height + j2)
+    x3 = (noise(offset + i + 5) * width + j3)
+    y3 = (noise(offset + i + 6) * height + j3)
+    x4 = (noise(offset + i + 7) * width + j4)
+    y4 = (noise(offset + i + 8) * height + j4)
 
-            if (choice == 0) {
-                stroke(r1, b1, g1)
-                bezier(x1, y1, x2, y2, x3, y3, x4, y4)
-                line(leftmargin, x1, x1, y1)
-                let max_right
-                if (y4 > rightmargin) {
-                    max_right = rightmargin
-                } else {
-                    max_right = y4
-                }
-                line(x4, y4, max_right, bottommargin)
-            }
-            else if (choice == 1) {
-                stroke(r1, g1, b1)
-                bezier(x1, y1, x2, y2, x3, y3, x4, y4)
-                stroke(r2, g2, b2)
-                line(x3, y3, rightmargin, y3)
-                line(x2, bottommargin, x2, y2)
-            }
-
-            offset += speed
-        }
+    stroke(255)
+    bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+    line(leftmargin, x1, x1, y1)
+    let max_right
+    if (y4 > rightmargin) {
+        max_right = rightmargin
     } else {
-        for (let j = 0; j < steps; j++) {
-            stroke(r1, g1, b1)
-            x1 = (noise(offset + i) * width + j1) * l
-            y1 = (noise(offset + i * 52) * height + j1) * l
-            x2 = (noise(offset + i * 53) * width + j2) * l - 100
-            y2 = (noise(offset + i * 54) * height + j2) * l - 100
-            x3 = (noise(offset + i * 55) * width + j3) * l + 100
-            y3 = (noise(offset + i * 56) * height + j3) * l + 100
-            x4 = (noise(offset + i * 57) * width + j4) * l
-            y4 = (noise(offset + i * 58) * height + j4) * l
-
-            bezier(x1, y1, x2, y2, x3, y3, x4, y4)
-
-            offset += speed
-        }
-        for (let j = 0; j < steps; j++) {
-            stroke(r2, g2, b2)
-            // stroke(0)
-            x1 = (noise(offset + i) * width + j1) * l - 150
-            y1 = (noise(offset + i * 2) * height + j1) * l + 150
-            x2 = (noise(offset + i * 3) * width + j2) * l
-            y2 = (noise(offset + i * 4) * height + j2) * l
-            x3 = (noise(offset + i * 5) * width + j3) * l
-            y3 = (noise(offset + i * 6) * height + j3) * l
-            x4 = (noise(offset + i * 7) * width + j4) * l + 150
-            y4 = (noise(offset + i * 8) * height + j4) * l + 150
-
-            bezier(x1, y1, x2, y2, x3, y3, x4, y4)
-            offset += speed
-        }
+        max_right = y4
     }
+    line(x4, y4, max_right, bottommargin)
+
+
+    offset += noise_speed
 }
 
-function draw_text() {
-    stroke(random(0, 255))
-    textSize(12)
-    textFont("Courier New")
-    text(`
-    for (let j = 0; j < steps; j++) {
-
-        stroke(r1, g1, b1)
-        x1 = (noise(offset + i) * width + j1) * l
-        y1 = (noise(offset + i + 2) * height + j1) * l
-        x2 = (noise(offset + i + 3) * width + j2) * l
-        y2 = (noise(offset + i + 4) * height + j2) * l
-        x3 = (noise(offset + i + 5) * width + j3) * l
-        y3 = (noise(offset + i + 6) * height + j3) * l
-        x4 = (noise(offset + i + 7) * width + j4) * l
-        y4 = (noise(offset + i + 8) * height + j4) * l
-
-        bezier(x1, y1, x2, y2, x3, y3, x4, y4)
-
-        offset += speed
+function mousePressed() {
+    if (pause == false) {
+        noLoop()
+        pause = true
+    } else {
+        loop()
+        pause = false
     }
-    `, random(100, windowWidth - 100), random(100, windowHeight - 150), 500)
 }
