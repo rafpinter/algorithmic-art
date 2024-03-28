19c880a35ef3a6969a02a89d45c457a49163cdd3
Rafaela Pinter <rpinter811@gmail.com>
2024-03-28 09:45:17 -0400

    default presentation mode

--- 07-vernissage/vernissage-code/bezier.js
+++ 07-vernissage/vernissage-code/bezier.js
@@ -4,6 +4,8 @@ References:
 https://www.youtube.com/watch?v=uctX1P3H3xM
 https://editor.p5js.org/PeterQwertz/sketches/Mh7aSsyMl
 https://github.com/rethread-studio/algorithmic-art-course/blob/main/examples/template-p5-to-svg.html
+
+p5.js
 */
 
 // Screen variables
@@ -17,7 +19,7 @@ var actualwidth
 var actualheight
 
 // Main variables
-let x1, y1, x2, y2, x3, y3, x4, y4
+let x1, y1, x2, y2, x3, y3, x4, y4 // points of the lines 
 let j1 = -100
 let j2 = -100
 let j3 = 100
@@ -25,7 +27,7 @@ let j4 = 100
 let i = 100
 
 // playground!!
-let offset = 10
+let offset = 100
 let noise_speed = 0.0055
 
 // Animation variables
@@ -46,6 +48,7 @@ function setup() {
     createCanvas(w, h)
     frameRate(20)
     mousePressed()
+    noFill() // No filling color 
 }
 
 // Drawing on the canvas
@@ -56,35 +59,36 @@ function draw() {
 // My bezier function
 function bezier_func() {
     // Configuration of the lines
-    noFill() // No filling color 
     stroke(255) // Color of the line. 255 = white
-    strokeWeight(2) // Weight of the line
+    strokeWeight(2) // Thickness of the line
 
     // Adding randomness to the points
     // In the original work, I "automatically" created all the lines with a "for" loop:
     // for (let j = 0; j < steps; j++) {
     // but here I'm showing one by one
-    x1 = (noise(offset + i) * width + j1)
-    y1 = (noise(offset + i + 2) * height + j1)
-    x2 = (noise(offset + i + 3) * width + j2)
-    y2 = (noise(offset + i + 4) * height + j2)
-    x3 = (noise(offset + i + 5) * width + j3)
-    y3 = (noise(offset + i + 6) * height + j3)
-    x4 = (noise(offset + i + 7) * width + j4)
-    y4 = (noise(offset + i + 8) * height + j4)
+    x1 = noise(offset + i) * width + j1
+    y1 = noise(offset + i + 2) * height + j1
+    x2 = noise(offset + i + 3) * width + j2
+    y2 = noise(offset + i + 4) * height + j2
+    x3 = noise(offset + i + 5) * width + j3
+    y3 = noise(offset + i + 6) * height + j3
+    x4 = noise(offset + i + 7) * width + j4
+    y4 = noise(offset + i + 8) * height + j4
 
     // Constructing Bezier curves with the points
     bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+    // What happens if we see only the bezier curves?
 
     // Adding straight line to an end of the Beziers
-    line(leftmargin, x1, x1, y1)
-    // But watching out for the margins
+    line(leftmargin, topmargin, x1, y1)
+    // Respecting the margins
     let max_right
     if (y4 > rightmargin) {
         max_right = rightmargin
     } else {
         max_right = y4
     }
+
     // Adding another line to another end of the Beziers
     line(x4, y4, max_right, bottommargin)
 
@@ -96,6 +100,8 @@ function bezier_func() {
     // In the original work, I also desabled the javascript loops, 
     // so the screen is not refreshed
     // noLoop()
+
+    // Now let's play with the code
 }
 
 
